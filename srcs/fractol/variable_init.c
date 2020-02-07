465c6096481a1cf8db8cefa1f24ae8db9f186ad3
RadioPotin <dariopinto@hotmail.fr>
2020-02-07 16:49:53 +0100

    forward with updated event, need work on centered zoom and adding fractal sets

--- srcs/fractol/variable_init.c
+++ srcs/fractol/variable_init.c
@@ -6,7 +6,7 @@
 /*   By: dapinto <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 17:30:40 by dapinto           #+#    #+#             */
-/*   Updated: 2020/02/07 11:18:36 by dapinto          ###   ########.fr       */
+/*   Updated: 2020/02/07 16:03:23 by dapinto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static double	set_x1(int fractal)
 {
-	double		x1_values[2];
+	double	x1_values[2];
 
 	x1_values[0] = -2.1;
 	x1_values[1] = 0.285;
@@ -23,7 +23,7 @@ static double	set_x1(int fractal)
 
 static double	set_y1(int fractal)
 {
-	double		y1_values[2];
+	double	y1_values[2];
 
 	y1_values[0] = -1.2;
 	y1_values[1] = 0.013;
@@ -50,7 +50,7 @@ static int	set_zoom(int fractal)
 
 double			set_trans_x(int fractal)
 {
-	double		trans_x_values[2];
+	double	trans_x_values[2];
 
 	trans_x_values[0] = -0.5;
 	trans_x_values[1] = -2.5;
@@ -59,7 +59,7 @@ double			set_trans_x(int fractal)
 
 double			set_trans_y(int fractal)
 {
-	double		trans_y_values[2];
+	double	trans_y_values[2];
 
 	trans_y_values[0] = 0;
 	trans_y_values[1] = -1.25;
@@ -68,7 +68,7 @@ double			set_trans_y(int fractal)
 
 void			initialize_variables(int fractal)
 {
-	t_fractol *frac;
+	t_fractol	*frac;
 
 	frac = fetchenv();
 	frac->x1 = set_x1(fractal);
