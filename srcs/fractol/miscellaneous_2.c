6176de3f7a2bd23343cdfd6982b3da9d685f288b
Dario Pinto <laradiopotin@gmail.com>
2021-01-06 16:26:06 +0100

    update 42headers and prepare menu

--- srcs/fractol/miscellaneous_2.c
+++ srcs/fractol/miscellaneous_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miscellaneous_2.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dapinto <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: dapinto <dapinto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/07 12:20:51 by dapinto           #+#    #+#             */
-/*   Updated: 2020/02/25 12:32:12 by dapinto          ###   ########.fr       */
+/*   Updated: 2021/01/06 16:24:48 by dapinto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,12 @@ t_fractalizer	*fractal_holder(int fractal)
 		hold[3] = &mandelbrot_flower;
 	}
 	return (&hold[fractal]);
+}
+
+void            menudisplay(void)
+{
+    t_fractol *f;
+
+    f = fetchenv();
+    
 }
