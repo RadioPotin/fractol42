31213358724e8a046faad6c1f2e1a1f12a586507
Dario Pinto <laradiopotin@gmail.com>
2021-01-14 11:40:55 +0100

    update norm

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: dapinto <dapinto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 12:23:03 by dapinto           #+#    #+#             */
-/*   Updated: 2021/01/13 12:37:31 by dapinto          ###   ########.fr       */
+/*   Updated: 2021/01/14 11:40:24 by dapinto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,20 @@
 typedef int			(*t_event)(int);
 typedef int			*(*t_palette)(void);
 
-typedef struct	s_eve
+typedef struct		s_eve
 {
-	int			key;
-	int			palet;
-	int			lock;
-}				t_eve;
+	int				key;
+	int				palet;
+	int				lock;
+}					t_eve;
 
 typedef struct		s_compute
 {
-	int		iter;
-	double	c_r;
-	double	c_i;
-	double	z_r;
-	double	z_i;
+	int				iter;
+	double			c_r;
+	double			c_i;
+	double			z_r;
+	double			z_i;
 }					t_comp;
 
 typedef struct		s_fractol
@@ -61,7 +61,7 @@ typedef struct		s_fractol
 	double			julia_r;
 	double			julia_i;
 	int				(**fractal_type)(int, t_comp *);
-	void 			(**fractal_compute)(int, int, struct s_fractol *, t_comp *);
+	void			(**fractal_compute)(int, int, struct s_fractol *, t_comp *);
 }					t_fractol;
 
 typedef int			(*t_fractalizer)(int, t_comp *);
@@ -70,72 +70,83 @@ typedef void		(*t_compute)(int, int, t_fractol *, t_comp *);
 typedef struct		s_thread
 {
 	pthread_t		id;
-	int 			n;
+	int				n;
 	int				top;
 	t_comp			var;
 	double			y;
 	t_fractol		*env;
 }					t_thread;
 
-/* Miscellaneous features */
+/*
+** Miscellaneous features
+*/
 
-char			**fractal_list(void);
-int				get_arguments(int argc, char **argv);
-void			usage(void);
-void			print_fractal_list(void);
+char				**fractal_list(void);
+int					get_arguments(int argc, char **argv);
+void				usage(void);
+void				print_fractal_list(void);
 
-/* Environment */
+/*
+** Environment
+*/
 
-t_fractol		*fetchenv(void);
+t_fractol			*fetchenv(void);
 
-/* Events and window details */
+/*
+** Events and window details
+*/
 
-int				do_nothing(int k);
-int				mouse_zm(int key, int x, int y);
-int				mouse_mvt(int x, int y);
-int				event_manager(int k);
-int				ft_cleanclose(int k);
-int				ft_translate(int k);
-int				ft_switch(int k);
-int				switch_palette(int k);
-int				ft_reset(int k);
-int				lockmvt(int k);
-void			clear_image(void);
-void            menudisplay(void);
+int					do_nothing(int k);
+int					mouse_zm(int key, int x, int y);
+int					mouse_mvt(int x, int y);
+int					event_manager(int k);
+int					ft_cleanclose(int k);
+int					ft_translate(int k);
+int					ft_switch(int k);
+int					switch_palette(int k);
+int					lockmvt(int k);
+int					ft_reset(int k);
+void				clear_image(void);
+void				menudisplay(void);
 
-int				*colour_tab(int palet);
-int 			*fire(void);
-int 			*miko_vanille_fraise(void);
-int				*yunowork(void);
-int				*are_you_ogay(void);
-int 			*bryce_canyon(void);
-int				*alberage(void);
+int					*colour_tab(int palet);
+int					*fire(void);
+int					*miko_vanille_fraise(void);
+int					*yunowork(void);
+int					*are_you_ogay(void);
+int					*bryce_canyon(void);
+int					*alberage(void);
 
-void			draw(void);
+void				draw(void);
 
-/* Fractals */
+/*
+** Fractals
+*/
+t_fractalizer		*fractal_holder(int fractal);
+int					julia(int maxiter, t_comp *var);
+int					mandelbrot(int maxiter, t_comp *var);
+int					burningship(int maxiter, t_comp *var);
+int					mandelbrot_flower(int maxiter, t_comp *var);
 
-t_fractalizer	*fractal_holder(int fractal);
-int				julia(int maxiter, t_comp *var);
-int				mandelbrot(int maxiter, t_comp *var);
-int				burningship(int maxiter, t_comp *var);
-int				mandelbrot_flower(int maxiter, t_comp *var);
+/*
+** Fractal variables init
+*/
 
-/* Fractal variables init */
+void				initialize_variables(int fractal_type);
+double				set_x1(int fractal);
+double				set_y1(int fractal);
+int					set_maxiter(int fractal);
+int					set_zoom(int fractal);
+double				set_trans_y(int fractal);
+double				set_trans_x(int fractal);
 
-void			initialize_variables(int fractal_type);
-double			set_x1(int fractal);
-double			set_y1(int fractal);
-int				set_maxiter(int fractal);
-int				set_zoom(int fractal);
-double			set_trans_y(int fractal);
-double			set_trans_x(int fractal);
+/*
+** Computation functions
+*/
 
-/* Computation functions */
-
-t_compute		*set_compute_struct(int fractal);
-void			set_comp_mandel(int x, int y, t_fractol *f, t_comp *var);
-void			set_comp_julia(int x, int y, t_fractol *f, t_comp *var);
-void			set_comp_burningship(int x, int y, t_fractol *f, t_comp *var);
+t_compute			*set_compute_struct(int fractal);
+void				set_comp_mandel(int x, int y, t_fractol *f, t_comp *var);
+void				set_comp_julia(int x, int y, t_fractol *f, t_comp *var);
+void				set_comp_burningship(int x, int y, t_fractol *f, t_comp *v);
 
 #endif
